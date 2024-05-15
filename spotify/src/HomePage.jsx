44d049cbcd99ffa1f4c03fdba5936a78ce927283
Samuelarputharaj <162418711+Samuelarputharaj@users.noreply.github.com>
2024-05-15 16:57:38 +0000

    Pending changes exported from your codespace

---
+++ spotify/src/HomePage.jsx
@@ -0,0 +1,175 @@
+import './HomePage.css'
+function HomePage() {
+    
+return(
+<>
+  <div className="big-container">
+    <div className="song-box1">
+      <div className="card-1" />
+      <div className="cont">
+        <div className="song">Ayayayo Anandhame-MassTamilan.com</div>
+        <marquee>
+          <div className="art">Haricharan-MassTamilan.com</div>
+        </marquee>
+      </div>
+    </div>
+    <div className="song-box2">
+      <div className="card-2" />
+      <div className="cont">
+        <div className="song">Chand Sifarish-MassTamilan.com</div>
+        <marquee>
+          <div className="art">Jatlin-Lalit,Prasoon Joshi-MassTamilan.com</div>
+        </marquee>
+      </div>
+    </div>
+    <div className="song-box3">
+      <div className="card-3" />
+      <div className="cont">
+        <div className="song">Damaku damaku-MassTamilan.com</div>
+        <marquee>
+          <div className="art">Benny Dayal-MassTamilan.com</div>
+        </marquee>
+      </div>
+    </div>
+    <div className="song-box4">
+      <div className="card-4" />
+      <div className="cont">
+        <div className="song">Dora Dora-MassTamilan.com</div>
+        <marquee>
+          <div className="art">Balram,Kalyani Nair-MassTamilan.com</div>
+        </marquee>
+      </div>
+    </div>
+    <div className="song-box5">
+      <div className="card-5" />
+      <div className="cont">
+        <div className="song">Idhu dhan-MassTamilan.com</div>
+        <marquee>
+          <div className="art">Naresh Iyer,Shasha-MassTamilan.com</div>
+        </marquee>
+      </div>
+    </div>
+    <div className="song-box6">
+      <div className="card-6" />
+      <div className="cont">
+        <div className="song">Lajjavathiye-MassTamilan.com</div>
+        <marquee>
+          <div className="art">Jessie Gift-MassTamilan.com</div>
+        </marquee>
+      </div>
+    </div>
+    <div className="song-box7">
+      <div className="card-7" />
+      <div className="cont">
+        <div className="song">Manike Hindi-MassTamilan.com</div>
+        <marquee>
+          <div className="art">
+            Tanisk Bagchi,Yohani,jubinNautial-MassTamilan.com
+          </div>
+        </marquee>
+      </div>
+    </div>
+    <div className="song-box8">
+      <div className="card-8" />
+      <div className="cont">
+        <div className="song">Mannipaya-MassTamilan.com</div>
+        <marquee>
+          <div className="art">
+            A.R.Rahman &amp; Shreya Gosal-MassTamilan.com
+          </div>
+        </marquee>
+      </div>
+    </div>
+    <div className="song-box9">
+      <div className="card-9" />
+      <div className="cont">
+        <div className="song">Mannipaya-MassTamilan.com</div>
+        <marquee>
+          <div className="art">
+            A.R.Rahman &amp; Shreya Gosal-MassTamilan.com
+          </div>
+        </marquee>
+      </div>
+    </div>
+    <div className="song-box10">
+      <div className="card-10" />
+      <div className="cont">
+        <div className="song">Mannipaya-MassTamilan.com</div>
+        <marquee>
+          <div className="art">
+            A.R.Rahman &amp; Shreya Gosal-MassTamilan.com
+          </div>
+        </marquee>
+      </div>
+    </div>
+    <div className="song-box11">
+      <div className="card-11" />
+      <div className="cont">
+        <div className="song">Mannipaya-MassTamilan.com</div>
+        <marquee>
+          <div className="art">
+            A.R.Rahman &amp; Shreya Gosal-MassTamilan.com
+          </div>
+        </marquee>
+      </div>
+    </div>
+    <div className="song-box12">
+      <div className="card-12" />
+      <div className="cont">
+        <div className="song">Mannipaya-MassTamilan.com</div>
+        <marquee>
+          <div className="art">
+            A.R.Rahman &amp; Shreya Gosal-MassTamilan.com
+          </div>
+        </marquee>
+      </div>
+    </div>
+    <div className="song-box13">
+      <div className="card-13" />
+      <div className="cont">
+        <div className="song">Mannipaya-MassTamilan.com</div>
+        <marquee>
+          <div className="art">
+            A.R.Rahman &amp; Shreya Gosal-MassTamilan.com
+          </div>
+        </marquee>
+      </div>
+    </div>
+    <div className="playlist">
+      <div className="card" />
+      <div className="cont-1">
+        <div className="song-1">
+          <a href="./styles/playlist.html">Tum Hi Ho-MassTamilan.com</a>
+        </div>
+        <marquee>
+          <div className="singer">Mithoon,Arijith Singh-MassTamilan.com</div>
+        </marquee>
+      </div>
+    </div>
+  </div>
+  <header>
+    <div className="circ">
+      <i className="fa-solid fa-s fa-3x" />
+    </div>
+    <div className="head-in">Music</div>
+    <div className="head-in1">Podcast</div>
+  </header>
+  <footer>
+    <div className="home">
+      <i className="fa-solid fa-house fa-2x" />
+      <br />
+    </div>
+    <div className="search">
+      <i className="fa-solid fa-magnifying-glass fa-2x" />
+    </div>
+    <div className="library">
+      <i className="fa-solid fa-list fa-2x" />
+    </div>
+    <div className="premium">
+      <i className="fa-brands fa-spotify fa-2x" />
+    </div>
+  </footer>
+</>
+)
+}
+export default HomePage;
